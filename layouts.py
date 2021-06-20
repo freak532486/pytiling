c9747aaa92fc1ef87aea13d4e9bc5507253e0967
freak532486 <maxwillich99@gmail.com>
2021-06-20 11:28:09 +0200

    Use proper inheritance for layouts

--- layouts.py
+++ layouts.py
@@ -3,17 +3,23 @@ from definitions import *
 class Layout:
 
     regions = []
+    windows = []
+
+    def __init__(self, windows, screen: Rect):
+        self.windows = windows
+        self.calculate_regions(screen)
+        self.assign_and_move()
 
     def __dist__(self, a, b):
         ax, ay = a
         bx, by = b
         return (ax - bx) * (ax - bx) + (ay - by) * (ay - by)
 
-    def __get_closest_window(self, rect: Rect, windows):
+    def __get_closest_window(self, rect: Rect):
         center = rect.center()
-        min_window = windows[0]
+        min_window = self.windows[0]
         min_dist = float("inf")
-        for window in windows:
+        for window in self.windows:
             window_rect = window.get_geometry()
             window_center = window_rect.center()
             if self.__dist__(center, window_center) < min_dist:
@@ -21,16 +27,35 @@ class Layout:
                 min_dist = self.__dist__(center, window_center)
         return min_window
 
-    def assign_and_move(self, windows):
-        for region in self.regions:
-            win = self.__get_closest_window(region, windows)
+    # Override this for new layouts
+    def calculate_regions(self, screen):
+        return [screen] * len(self.windows)
+
+    def assign_and_move(self):
+        new_windows = []
+        for i in range(len(self.regions)):
+            region = self.regions[i]
+            win = self.__get_closest_window(region)
             win.move_resize(region)
-            windows.remove(win)
+            new_windows.append(win)
+            self.windows.remove(win)
+        self.windows = new_windows
 
 class MasterSlaveDivider(Layout):
 
-    def __init__(self, screen: Rect, n, gaps = 0, master_ratio = 0.5):
-        super()
+    gaps = None
+    master_ratio = None
+
+    def __init__(self, windows, screen: Rect, gaps = 0, master_ratio = 0.5):
+        self.gaps = gaps
+        self.master_ratio = master_ratio
+        super().__init__(windows, screen)
+        
+
+    def calculate_regions(self, screen):
+        n = len(self.windows)
+        gaps = self.gaps
+        master_ratio = self.master_ratio
         if n == 1:
             return [Rect(gaps, gaps, screen.width - 2 * gaps, screen.height - 2 * gaps)]
         # Now that the divide by 0 is out of the way...
@@ -44,8 +69,16 @@ class MasterSlaveDivider(Layout):
 
 class ColumnDivider(Layout):
 
-    def __init__(self, screen: Rect, n, gaps = 0):
-        ret = []
+    gaps = None
+
+    def __init__(self, windows, screen: Rect, gaps = 0):
+        self.gaps = gaps
+        super().__init__(windows, screen)
+        
+
+    def calculate_regions(self, screen):
+        n = len(self.windows)
+        gaps = self.gaps
         height = screen.height - 2 * gaps
         width = (screen.width - (n + 1) * gaps) / n
         for i in range(n):
--- pytile.py
+++ pytile.py
@@ -6,5 +6,5 @@ windows = Window.get_all(workspace = current_workspace)
 root_size = Window.get_root_size(current_workspace)
 root_geom = Rect(0, 0, root_size[0], root_size[1])
 
-divider = layouts.MasterSlaveDivider(root_geom, len(windows), gaps = 20)
-divider.assign_and_move(windows)
+divider = layouts.MasterSlaveDivider(windows, root_geom, gaps = 20)
+divider.assign_and_move()
