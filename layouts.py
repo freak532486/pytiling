50c19409132074620b31a35ef98df2ef51696e74
freak532486 <maxwillich99@gmail.com>
2021-06-20 11:06:57 +0200

    Put all layout code into layout (super)-class

--- layouts.py
+++ layouts.py
@@ -1,30 +1,52 @@
 from definitions import *
 
-class MasterSlaveDivider:
+class Layout:
 
-    def divide(self, rect: Rect, n, gaps = 0, master_ratio = 0.5):
+    regions = []
+
+    def __dist__(self, a, b):
+        ax, ay = a
+        bx, by = b
+        return (ax - bx) * (ax - bx) + (ay - by) * (ay - by)
+
+    def __get_closest_window(self, rect: Rect, windows):
+        center = rect.center()
+        min_window = windows[0]
+        min_dist = float("inf")
+        for window in windows:
+            window_rect = window.get_geometry()
+            window_center = window_rect.center()
+            if self.__dist__(center, window_center) < min_dist:
+                min_window = window
+                min_dist = self.__dist__(center, window_center)
+        return min_window
+
+    def assign_and_move(self, windows):
+        for region in self.regions:
+            win = self.__get_closest_window(region, windows)
+            win.move_resize(region)
+            windows.remove(win)
+
+class MasterSlaveDivider(Layout):
+
+    def __init__(self, screen: Rect, n, gaps = 0, master_ratio = 0.5):
+        super()
         if n == 1:
-            return [Rect(gaps, gaps, rect.width - 2 * gaps, rect.height - 2 * gaps)]
+            return [Rect(gaps, gaps, screen.width - 2 * gaps, screen.height - 2 * gaps)]
         # Now that the divide by 0 is out of the way...
-        ret = []
-        master_width = rect.width * master_ratio - 1.5 * gaps
-        slave_width = rect.width * (1 - master_ratio) - 1.5 * gaps
-        master_height = rect.height - 2 * gaps
-        slave_height = (rect.height - n * gaps) / (n - 1)
-
-        ret.append(Rect(gaps, gaps, master_width, master_height))
+        master_width = screen.width * master_ratio - 1.5 * gaps
+        slave_width = screen.width * (1 - master_ratio) - 1.5 * gaps
+        master_height = screen.height - 2 * gaps
+        slave_height = (screen.height - n * gaps) / (n - 1)
+        self.regions.append(Rect(gaps, gaps, master_width, master_height))
         for i in range(n - 1):
-            ret.append(Rect(2 * gaps + master_width, gaps + (slave_height + gaps) * i, slave_width, slave_height))
-        return ret
+            self.regions.append(Rect(2 * gaps + master_width, gaps + (slave_height + gaps) * i, slave_width, slave_height))
 
-class ColumnDivider:
+class ColumnDivider(Layout):
 
-    def divide(self, rect: Rect, n, gaps = 0):
+    def __init__(self, screen: Rect, n, gaps = 0):
         ret = []
-        height = rect.height - 2 * gaps
-        width = (rect.width - (n + 1) * gaps) / n
-        current_x = gaps
+        height = screen.height - 2 * gaps
+        width = (screen.width - (n + 1) * gaps) / n
         for i in range(n):
-            ret.append(Rect(int(current_x), gaps, int(width), height))
-            current_x += width + gaps
-        return ret
+            self.regions.append(Rect(int(gaps + (width + gaps) * i), gaps, int(width), height))
--- pytile.py
+++ pytile.py
@@ -1,32 +1,10 @@
 from definitions import *
 import layouts
 
-def dist(a, b):
-    ax, ay = a
-    bx, by = b
-    return (ax - bx) * (ax - bx) + (ay - by) * (ay - by)
-
-def getClosestWindow(rect: Rect, windows):
-    center = rect.center()
-    min_window = windows[0]
-    min_dist = float("inf")
-    for window in windows:
-        window_rect = window.get_geometry()
-        window_center = window_rect.center()
-        if dist(center, window_center) < min_dist:
-            min_window = window
-            min_dist = dist(center, window_center)
-    return min_window
-
 current_workspace = Window.get_current_workspace()
 windows = Window.get_all(workspace = current_workspace)
 root_size = Window.get_root_size(current_workspace)
 root_geom = Rect(0, 0, root_size[0], root_size[1])
 
-divider = layouts.MasterSlaveDivider()
-
-divide = divider.divide(root_geom, len(windows), gaps = 20)
-for region in divide:
-    win = getClosestWindow(region, windows)
-    win.move_resize(region)
-    windows.remove(win)
+divider = layouts.MasterSlaveDivider(root_geom, len(windows), gaps = 20)
+divider.assign_and_move(windows)
